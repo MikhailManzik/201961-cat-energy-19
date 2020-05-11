dc2d096e21670e262c1f349161d74978aeb62672
Mikhail Manzik <mikhailmanzik@yandex.ru>
2020-05-11 22:52:08 +0200

    Исправялет замечания.

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -3,19 +3,19 @@ var buttonIcon = document.querySelector(".toggle__icon");
 var siteNavigation = document.querySelector(".main-nav");
 
 buttonMenu.addEventListener("click", function () {
-  if (buttonIcon.classList.contains("toggle__icon-opened")) {
-    buttonIcon.classList.remove("toggle__icon-opened");
-    buttonIcon.classList.add("toggle__icon-closed");
+  if (buttonIcon.classList.contains("toggle__icon--opened")) {
+    buttonIcon.classList.remove("toggle__icon--opened");
+    buttonIcon.classList.add("toggle__icon--closed");
   } else {
-    buttonIcon.classList.remove("toggle__icon-closed");
-    buttonIcon.classList.add("toggle__icon-opened");
-    }
+    buttonIcon.classList.remove("toggle__icon--closed");
+    buttonIcon.classList.add("toggle__icon--opened");
+  }
 });
 
 buttonMenu.addEventListener("click", function () {
-  if (siteNavigation.classList.contains("main-nav-closed")) {
-    siteNavigation.classList.remove("main-nav-closed");
+  if (siteNavigation.classList.contains("main-nav--closed")) {
+    siteNavigation.classList.remove("main-nav--closed");
   } else {
-    siteNavigation.classList.add("main-nav-closed");
-    }
+    siteNavigation.classList.add("main-nav--closed");
+  }
 })
